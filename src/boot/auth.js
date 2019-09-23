c1a94e4a2747166272b8088d481f9b833952c829
vikramide <vikrambanand.ide@gmail.com>
2019-09-23 23:44:39 +0530

    Added the Keycloak plugin

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -1,54 +1,32 @@
-// import * as Keycloak from 'keycloak-js'
-
-// // keycloak init options
-// let initOptions = {
-//   url: 'http://localhost:8080/auth',
-//   realm: 'hs-auth',
-//   clientId: 'hs-playground',
-//   onLoad: 'check-sso'
-// }
-
-// let keycloak = Keycloak(initOptions)
-
-// export default async ({ Vue }) => {
-//   Vue.prototype.$keycloak = keycloak
-//   keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
-//     console.log('auth')
-//     console.log(auth)
+import VueKeycloakJs from '@dsb-norge/vue-keycloak-js'
+// import axios from './axios'
+import Vue from 'vue'
+// function tokenInterceptor () {
+//   axios.interceptors.request.use(config => {
+//     config.headers.Authorization = `Bearer ${Vue.prototype.$keycloak.token}`
+//     return config
+//   }, error => {
+//     return Promise.reject(error)
 //   })
 // }
 
-import VueKeycloakJs from '@dsb-norge/vue-keycloak-js'
-import axios from './axios'
-import Vue from 'vue'
-function tokenInterceptor () {
-  axios.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${Vue.prototype.$keycloak.token}`
-    return config
-  }, error => {
-    return Promise.reject(error)
+let ready = new Promise(resolve => {
+  Vue.use(VueKeycloakJs, {
+    init: {
+      // Use 'login-required' to always require authentication
+      // If using 'login-required', there is no need for the router guards in router.js
+      onLoad: 'check-sso'
+    },
+    config: {
+      url: 'http://localhost:8080/auth',
+      realm: 'hypermine',
+      clientId: 'hs-playground'
+    },
+    onReady: resolve
   })
-}
-
-Vue.use(VueKeycloakJs, {
-  init: {
-    // Use 'login-required' to always require authentication
-    // If using 'login-required', there is no need for the router guards in router.js
-    onLoad: 'check-sso'
-  },
-  config: {
-    url: 'http://localhost:8080/auth',
-    realm: 'hs-auth',
-    clientId: 'hs-playground'
-  },
-  onReady: (keycloak) => {
-    tokenInterceptor()
-    /* eslint-disable no-new */
-    // new Vue({
-    //   el: '#app',
-    //   router,
-    //   template: '<App/>',
-    //   render: h => h(App)
-    // })
-  }
 })
+
+export default async function ({ Vue, store }) {
+  await ready
+  // tokenInterceptor()
+}
